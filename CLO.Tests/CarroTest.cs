02c4c7b8ba6ef7d698b783f7190abca84f28bba8
Luizh042 <coisaqualquer996@gmail.com>
2023-05-24 19:41:34 -0300

    exersicio 7 de c#

--- CLO.Tests/CarroTest.cs
+++ CLO.Tests/CarroTest.cs
@@ -18,5 +18,40 @@ public class Tests
         Assert.AreEqual("Volkswagen", carro.Marca);
         Assert.AreEqual("Parati",carro.Modelo);
         Assert.AreEqual(2001, carro.Ano);
+    }   
+
+
+
+
+    [TestFixture]
+    public class TestCarro1
+    {
+        [Test]
+        public void CarroEletrico_AutonomiaBateria_Correta()
+        {
+            var carroEletrico = new CarroEletrico
+            {
+                Marca = "Tesla",
+                Modelo = "Model S",
+                Ano = 2020,
+                AutonomiaBateria = 600
+            };
+            var autonomia = carroEletrico.AutonomiaBateria;
+            Assert.AreEqual(600, autonomia);
+        }
+        [Test]
+        public void CarroEsportivo_Aceleracao_Correta()
+        {
+            var carroEsportivo = new CarroEsportivo
+            {
+                Marca = "Ferrari",
+                Modelo = "488 GTB",
+                Ano = 2020,
+            };
+           carroEsportivo.AcelerarRapidamente();
+           double velocidade = carroEsportivo.Velocidade;
+
+            Assert.IsTrue(velocidade > 50);
+        }
     }
 }
--- CLO/Carro.cs
+++ CLO/Carro.cs
@@ -7,4 +7,22 @@
         public string Modelo {get; set;}
         public int Ano {get; set;}
     }
+    public class CarroEletrico : Carro
+    {
+        public double AutonomiaBateria {get; set; }
+        public string Marca {get; set;}
+        public string Modelo {get; set;}
+        public int Ano {get; set;}
+    }
+    public class CarroEsportivo : Carro
+    {
+        public void AcelerarRapidamente()
+        {
+            Velocidade += 100;
+        }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public double Velocidade { get; set; }
+    }
 }
